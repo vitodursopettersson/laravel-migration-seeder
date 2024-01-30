dc520ad9f29bf51691a28388048e99bb652c578b
vitodursopettersson <vitodurso98@gmail.com>
2024-01-30 17:09:46 +0100

    first commit

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use App\Models\Train;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        $trains = Train::all();
+        return view('welcome', compact('trains'));
+    }
+};
