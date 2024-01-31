0ee9017ff20957dc65daee1ed207fe236d69327f
vitodursopettersson <vitodurso98@gmail.com>
2024-01-31 09:17:00 +0100

    print trains foreach from db

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,6 @@
 @php
     use Carbon\Carbon;
+    $dt = Carbon::now();
 @endphp
 
 <!DOCTYPE html>
@@ -15,11 +16,17 @@
 <body>
 
     <div class="container">
+        <h1>Prossimi treni:</h1>
         @foreach ($trains as $train)
-            @if ($train['giorno_di_partenza'] >= Carbon::now())
+            @if (Carbon::parse($train['giorno_di_partenza'])->gt($dt))
                 <h1>{{ $train['azienda'] }}</h1>
-                <h2>{{ $train['azienda'] }}</h2>
-                <h2>{{ $train['azienda'] }}</h2>
+                <h2>stazione di partenza:{{ $train['stazione_partenza'] }}</h2>
+                <h3>stazione di arrivo:{{ $train['stazione_arrivo'] }}</h3>
+                <h4>giorno di partenza:{{ $train['giorno_di_partenza'] }}</h4>
+                <h5>orario di partenza: {{ $train['orario_partenza'] }}</h5>
+                <h5>orario di arrivo:{{ $train['orario_arrivo'] }}</h5>
+                <h6>codice treno:{{ $train['codice_treno'] }}</h6>
+                <span>carrozze treno:{{ $train['carrozze_treno'] }}</span>
             @endif
         @endforeach
     </div>
